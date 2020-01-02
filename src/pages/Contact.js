3c6a485ff6b16d5eac5849500b9207917ad48fda
Rob Spears <rob.sp3ars@gmail.com>
2020-01-02 17:30:35 -0500

    Contact page complete

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Icon } from "semantic-ui-react";
+import "./Contact.css";
 
 export class Contact extends Component {
   componentDidMount = () => {
@@ -7,6 +9,39 @@ export class Contact extends Component {
   };
 
   render() {
-    return <p>This is the Contact page</p>;
+    return (
+      <React.Fragment>
+        <div id="Contact-social">
+          <a
+            href="https://www.linkedin.com/in/robsp3ars"
+            alt="LinkedIn"
+            title="LinkedIn"
+          >
+            <Icon name="linkedin" size="huge" className="socialIcon" />
+          </a>
+          <a
+            href="https://www.gitlab.com/robsp3ars"
+            alt="GitLab"
+            title="GitLab"
+          >
+            <Icon name="gitlab" size="huge" className="socialIcon" />
+          </a>
+          <a
+            href="https://github.com/forty9unbeaten"
+            alt="GitHub"
+            title="GitHub"
+          >
+            <Icon name="github" size="huge" className="socialIcon" />
+          </a>
+        </div>
+        <p id="Contact-phone" alt="3179419088" title="Phone: (317) 941-9088">
+          Phone: (317) 941-9088
+        </p>
+        <p id="Contact-email">
+          Send an email to Rob at{" "}
+          <a href="mailto: rob.sp3ars@gmail.com">rob.sp3ars@gmail.com</a>
+        </p>
+      </React.Fragment>
+    );
   }
 }
