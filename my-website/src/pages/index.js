0823e583c04be65fb79c587cd5caa00db0668bd9
Rob Spears <rob.sp3ars@gmail.com>
2020-01-02 20:18:33 +0000

    Deleted my-website/public/index.html, my-website/src/components/App.css, my-website/src/components/App.js, my-website/src/components/Navbar.css, my-website/src/components/Navbar.js, my-website/src/components/index.js, my-website/src/img/background.jpg, my-website/src/pages/Contact.js, my-website/src/pages/Home.js, my-website/src/pages/NotFound.js, my-website/src/pages/Portfolio.js, my-website/src/pages/Resume.js, my-website/src/pages/index.js, my-website/src/index.css, my-website/src/index.js, my-website/src/routes.js, my-website/.gitignore, my-website/README.md, my-website/package-lock.json, my-website/package.json files

--- my-website/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { Home } from "./Home";
-export { Resume } from "./Resume";
-export { Portfolio } from "./Portfolio";
-export { Contact } from "./Contact";
-export { NotFound } from "./NotFound";
--- my-website/src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Home, Resume, Portfolio, Contact, NotFound } from "./pages";
-
-export const routes = [
-  { path: "/", component: Home },
-  { path: "/resume", component: Resume },
-  { path: "/portfolio", component: Portfolio },
-  { path: "/contact", component: Contact },
-  { path: "*", component: NotFound }
-];
